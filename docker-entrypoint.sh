a497dc95f7b2130d7804d893817d00df539d427e
Mark Gillespie <mark.gillespie@gmail.com>
2021-02-26 09:17:16 +0000

    Debug more

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,8 @@
 
 uname -a
 
-if [ -n "$SOURCE_PASSWORD" ]; then
-    sed -i "s/<password>[^<]*<\/password>/<password>$SOURCE_PASSWORD<\/password>/g" /etc/rsas.xml
-fi
-if [ -n "$SOURCE_USERNAME" ]; then
-    sed -i "s/<username>[^<]*<\/username>/<username>$SOURCE_USERNAME<\/username>/g" /etc/rsas.xml
-fi
-if [ -n "$MAX_CLIENTS" ]; then
-    sed -i "s/<clients>[^<]*<\/clients>/<clients>$MAX_CLIENTS<\/clients>/g" /etc/rsas.xml
-fi
-
-./usr/bin/rsas -b -c /etc/rsas.xml                                                                                                                   
-                                                                                                                                                        
 bash
-/app/liquidsoap /tmp/test.lic
-exec "$@"
+
+#./usr/bin/rsas -b -c /etc/rsas.xml
+#/app/liquidsoap /tmp/test.lic
+#exec "$@"
