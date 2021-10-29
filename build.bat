a47419a284e303ec952223f43f888a199d5eeba0
Mark Gillespie <mark.gillespie@gmail.com>
2021-10-29 18:41:08 +0100

    Renamed stuff

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 docker system prune
-docker build -t mgillespie/rocketaudio:testing2 -f- . < Dockerfile
-docker run --rm -it --entrypoint=/bin/bash mgillespie/rocketaudio:testing2
+docker build -t mgillespie/liquidsoap_icecast_kh:latest -f- . < Dockerfile
+docker run --rm -it --entrypoint=/bin/bash mgillespie/liquidsoap_icecast_kh:latest
