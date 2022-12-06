044e9dd74068149e884af9a372fb40cf94a5d2d3
Mark Gillespie <mark.gillespie@gmail.com>
2022-12-06 09:06:48 +0000

    LS213

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 docker system prune
-docker build -t mgillespie/liquidsoap_icecast_kh:v2.1.2 -f- . < Dockerfile
-docker run --rm -it --entrypoint=/bin/bash mgillespie/liquidsoap_icecast_kh:v2.1.2
+docker build -t mgillespie/liquidsoap_icecast_kh:v2.1.3 -f- . < Dockerfile
+docker run --rm -it --entrypoint=/bin/bash mgillespie/liquidsoap_icecast_kh:v2.1.3
 
 docker build -t mgillespie/liquidsoap_icecast_kh:latest -f- . < Dockerfile
 docker run --rm -it --entrypoint=/bin/bash mgillespie/liquidsoap_icecast_kh:latest
--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
-docker push mgillespie/liquidsoap_icecast_kh:v2.1.2
+docker push mgillespie/liquidsoap_icecast_kh:v2.1.3
 docker push mgillespie/liquidsoap_icecast_kh:latest
